79416b625261865ebea3d101697cd5dad5a94a0d
Алексей <shcherbakov.032005@gmail.com>
2022-01-08 13:08:14 +0300

    промежуточная точка сохранения ( пока что мультиплеер не робит)

--- client.py
+++ client.py
@@ -8,10 +8,10 @@ win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
 
-def redrawWindow(win,player, player2):
-    win.fill((255,255,255))
-    player.draw(win)
-    player2.draw(win)
+def redrawWindow(window, player, player2):
+    window.fill((255, 255, 255))
+    player.draw(window)
+    player2.draw(window)
     pygame.display.update()
 
 
@@ -32,5 +32,6 @@ def main():
 
         p.move()
         redrawWindow(win, p, p2)
+
 
 main()
--- server.py
+++ server.py
@@ -2,6 +2,15 @@ import socket
 from _thread import *
 from new_features import Player
 import pickle
+import pygame
+
+clock = pygame.time.Clock()
+width = 500
+height = 500
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("server")
+window.fill((255, 255, 255))
+
 
 server = "192.168.1.80"
 port = 5555
@@ -19,14 +28,20 @@ print("Waiting for a connection, Server Started")
 players = [Player(0, 0, 50, 50, (255, 0, 0)), Player(100, 100, 50, 50, (0, 0, 255))]
 
 
+
 def threaded_client(conn, player):
     conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
+            clock.tick(60)
             data = pickle.loads(conn.recv(2048))
             players[player] = data
-
+            players[0].move()
+            window.fill((255, 255, 255))
+            players[0].draw(window)
+            players[1].draw(window)
+            pygame.display.update()
             if not data:
                 print("Disconnected")
                 break
@@ -38,7 +53,6 @@ def threaded_client(conn, player):
 
                 print("Received: ", data)
                 print("Sending : ", reply)
-
             conn.sendall(pickle.dumps(reply))
         except:
             break
