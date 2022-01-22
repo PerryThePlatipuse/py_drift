94d89a1f50433daea3ab45d1fc90c82a10cdf885
PerryThePlatipuse <77574605+PerryThePlatipuse@users.noreply.github.com>
2022-01-22 17:20:12 +0300

    добавлен мультиплеер

---
+++ client.py
@@ -0,0 +1,37 @@
+import pygame
+from network import Network
+from new_features import Player
+
+width = 500
+height = 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+
+def redrawWindow(window, player, player2):
+    window.fill((255, 255, 255))
+    player.draw(window)
+    player2.draw(window)
+    pygame.display.update()
+
+
+def main():
+    run = True
+    n = Network()
+    p = n.getP()
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        p2 = n.send(p)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        p.move()
+        redrawWindow(win, p, p2)
+
+
+main()
